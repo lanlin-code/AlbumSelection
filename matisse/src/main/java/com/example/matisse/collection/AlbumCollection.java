025653a0ca788155d23e6d2e95773b5e20dc4bbf
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-08-10 16:49:26 +0800

    collection

--- matisse/src/main/java/com/example/matisse/collection/AlbumCollection.java
+++ matisse/src/main/java/com/example/matisse/collection/AlbumCollection.java
@@ -1,4 +1,83 @@
 package com.example.matisse.collection;
 
-public class AlbumCollection {
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import com.example.matisse.loader.AlbumLoader;
+
+import java.lang.ref.WeakReference;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+public class AlbumCollection implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final int FIRST = 1;
+    private AlbumCallback mAlbumCallback;
+    private WeakReference<Context> mContext;
+    private boolean finished = false;
+    private LoaderManager mLoaderManager;
+    private int mCurrentPosition = 0;
+
+    public void onCreate(FragmentActivity activity, AlbumCallback albumCallback) {
+        mContext = new WeakReference<Context>(activity);
+        mAlbumCallback = albumCallback;
+        mLoaderManager = LoaderManager.getInstance(activity);
+    }
+
+    public void loadAlbum() {
+        mLoaderManager.initLoader(FIRST, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Context context = mContext.get();
+        if (context == null) return null;
+        finished = false;
+        return AlbumLoader.newInstance(context);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        Context context = mContext.get();
+        if (context == null) return;
+        if (!finished) {
+            finished = true;
+            mAlbumCallback.onAlbumFinish(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        if (mContext.get() == null) return;
+        mAlbumCallback.onAlbumReset();
+    }
+
+    public void setCurrentPosition(int position) {
+        mCurrentPosition = position;
+    }
+
+    public int getCurrentPosition() {
+        return mCurrentPosition;
+    }
+
+    public void onDestroy() {
+        mAlbumCallback = null;
+        mLoaderManager.destroyLoader(FIRST);
+    }
+
+
+    public interface AlbumCallback {
+
+        void onAlbumFinish(Cursor cursor);
+
+        void onAlbumReset();
+
+    }
+
 }
--- matisse/src/main/java/com/example/matisse/collection/AlbumMediaCollection.java
+++ matisse/src/main/java/com/example/matisse/collection/AlbumMediaCollection.java
@@ -1,4 +1,78 @@
 package com.example.matisse.collection;
 
-public class AlbumMediaCollection {
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import com.example.matisse.entity.Album;
+import com.example.matisse.loader.AlbumMediaLoader;
+
+import java.lang.ref.WeakReference;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+public class AlbumMediaCollection implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final int SECOND = 2;
+    private AlbumMediaCallback mCallback;
+    private WeakReference<Context> mContext;
+    private LoaderManager mLoaderManager;
+    private boolean finished = false;
+    private final String ALBUM_KEY = "album";
+
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        if (mContext.get() == null || args == null) return null;
+        Album album = args.getParcelable(ALBUM_KEY);
+        if (album == null) return null;
+        finished = false;
+        return AlbumMediaLoader.newInstance(mContext.get(), album);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if (mContext.get() == null) return;
+        if (!finished) {
+            finished = true;
+            if (mCallback != null) mCallback.onFinished(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        if (mContext == null) return;
+        if (mCallback != null) mCallback.onReset();
+    }
+
+    public void onCreate(FragmentActivity activity, AlbumMediaCallback callback) {
+        mContext = new WeakReference<Context>(activity);
+        mCallback = callback;
+        mLoaderManager = LoaderManager.getInstance(activity);
+    }
+
+    public void loadItems(Album album) {
+        if (mContext.get() == null) return;
+        Bundle args = new Bundle();
+        args.putParcelable(ALBUM_KEY, album);
+        mLoaderManager.initLoader(SECOND, args, this);
+    }
+
+    public void onDestroy() {
+        mCallback = null;
+        mLoaderManager.destroyLoader(SECOND);
+    }
+
+
+    public interface AlbumMediaCallback {
+        void onFinished(Cursor cursor);
+
+        void onReset();
+    }
+
 }
