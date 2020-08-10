4762e572869680ebe8f3d29207272a34929ff777
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-08-10 17:03:12 +0800

    matisse util

--- matisse/src/main/java/com/example/matisse/executor/MyThreadPool.java
+++ matisse/src/main/java/com/example/matisse/executor/MyThreadPool.java
@@ -1,4 +1,27 @@
 package com.example.matisse.executor;
 
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class MyThreadPool {
+    private static ThreadPoolExecutor threadPoolExecutor;
+
+
+    public static void newInstance() {
+        int size = 4;
+        if (threadPoolExecutor == null)
+            threadPoolExecutor = new ThreadPoolExecutor(size, size, 0L,
+                    TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>());
+    }
+
+    public static void newInstance(int size) {
+        if (threadPoolExecutor == null)
+            threadPoolExecutor = new ThreadPoolExecutor(size, size, 0L,
+                    TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>());
+    }
+
+    public static void execute(Runnable runnable) {
+        threadPoolExecutor.execute(runnable);
+    }
 }
--- matisse/src/main/java/com/example/matisse/util/ThreadAdjust.java
+++ matisse/src/main/java/com/example/matisse/util/ThreadAdjust.java
@@ -1,4 +1,12 @@
 package com.example.matisse.util;
 
+import android.os.Handler;
+import android.os.Looper;
+
 public class ThreadAdjust {
+    private static Handler handler = new android.os.Handler(Looper.getMainLooper());
+
+    public static void post(Runnable r) {
+        handler.post(r);
+    }
 }
